0d3605e05d313a078a58ba799b69689b5f6bbca6
FVREED <73734920+FaridDadashzade@users.noreply.github.com>
2021-10-30 06:27:00 +0400

    C Y B Ξ R

---
+++ userbot/modules/botfather.py
@@ -0,0 +1,45 @@
+# CYBERUSERBOT - FARIDDADASHZADE #
+
+import os
+from telethon.tl.functions.contacts import UnblockRequest
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from userbot.events import register
+from userbot.cmdhelp import CmdHelp
+from telethon import events
+
+chat = "@BotFather"
+
+@register(outgoing=True, pattern="^.botfather ?(.*)")
+async def _(event):
+    if event.pattern_match.group(1):
+        text, username= event.pattern_match.group(1).split()
+        
+    else:
+        await event.edit("`Botu yaratmaq üçün <bot_adı><bot_istifadəçi_adı> qeyd edin.`")
+        return
+
+    async with event.client.conversation("@BotFather") as conv:
+        try:
+            await conv.send_message("/newbot")
+            audio = await conv.get_response()
+            await conv.send_message(text)
+            audio = await conv.get_response()
+            await conv.send_message(username)
+            audio = await conv.get_response()
+            await event.client.forward_messages("me", audio)
+            await event.edit('Məlumatlar kayıtlı mesajlara qeyd edildi.')
+        except YouBlockedUserError:
+            await event.client(UnblockRequest("93372553"))
+            await conv.send_message("/newbot")
+            audio = await conv.get_response()
+            await conv.send_message(text)
+            audio = await conv.get_response()
+            await conv.send_message(username)
+            audio = await conv.get_response()
+            await event.client.forward_messages("me", audio)
+            await event.edit('Məlumatlar kayıtlı mesajlara qeyd edildi.')
+            await event.delete()
+
+
+Help = CmdHelp('botfather')
+Help.add_command("botfather", "<bot_adı><bot_istifadəçi_adı>", "Bot yaratmağınıza kömək edər.").add()
