2a416bfc808f5736d0c375467aee57174d2c498b
FVREED <73734920+FaridDadashzade@users.noreply.github.com>
2021-10-30 06:27:30 +0400

    C Y B Ξ R

---
+++ userbot/events.py
@@ -0,0 +1,231 @@
+# Copyright (C) 2021 FaridDadashzade.
+#
+# CyberUserBot - FaridDadashzade
+
+""" CYBERUSERBOT """
+
+import sys
+from asyncio import create_subprocess_shell as asyncsubshell
+from asyncio import subprocess as asyncsub
+from os import remove
+from time import gmtime, strftime
+from pathlib import Path
+import re
+import inspect
+from traceback import format_exc
+
+from telethon import events
+
+from userbot import bot, BOTLOG_CHATID, CYBER_VERSION, LOGSPAMMER, PATTERNS, JARVIS, MYID, SUDO_ID
+
+
+def register(**args):
+    #stack = inspect.stack()
+    #cyber_stack_frame = stack[1]
+    #file_test = Path(cyber_stack_frame.filename)
+    #file_test = file_test.stem.replace(".py", "")
+    pattern = args.get('pattern', None)
+    disable_edited = args.get('disable_edited', False)
+    groups_only = args.get('groups_only', False)
+    insecure = args.get("insecure", False)
+    cyber = args.get('cyber', False)
+    jarvis = args.get('jarvis', False)
+    trigger_on_fwd = args.get('trigger_on_fwd', False)
+    trigger_on_inline = args.get('trigger_on_inline', False)
+    disable_errors = args.get('disable_errors', False)
+    sudo = args.get('sudo', False)
+
+    if pattern:
+        args["pattern"] = pattern.replace("^.", "^["+ PATTERNS + "]")
+    if "disable_edited" in args:
+        del args['disable_edited']
+
+    if "ignore_unsafe" in args:
+        del args['ignore_unsafe']
+
+    if "groups_only" in args:
+        del args['groups_only']
+    
+    if "insecure" in args:
+        del args["insecure"]  
+
+    if "disable_errors" in args:
+        del args['disable_errors']
+
+    if "trigger_on_fwd" in args:
+        del args['trigger_on_fwd']
+      
+    if "trigger_on_inline" in args:
+        del args['trigger_on_inline']
+        
+    if 'cyber' in args:
+        del args['cyber']
+        args['outgoing'] = True      
+        
+    if 'jarvis' in args:
+        del args['jarvis']
+        args['incoming'] = True
+        args["from_users"] = JARVIS
+        
+    if 'sudo' in args:
+        del args['sudo']
+        args['incoming'] = True
+        args["from_users"] = SUDO_ID
+        
+
+    def decorator(func):
+        async def wrapper(check):
+            if not LOGSPAMMER:
+                send_to = check.chat_id
+            else:
+                send_to = BOTLOG_CHATID
+
+            if not trigger_on_fwd and check.fwd_from:
+                return
+
+            if check.via_bot_id and not trigger_on_inline:
+                return
+             
+            if groups_only and not check.is_group:
+                await check.respond("`Bunun bir qrup olduğunu düşünmürəm!`")
+                return
+
+            try:
+                await func(check)
+                
+
+            except events.StopPropagation:
+                raise events.StopPropagation
+            except KeyboardInterrupt:
+                pass
+            except BaseException:
+                if not disable_errors:
+                    date = strftime("%d-%m-%Y", gmtime())
+
+                    eventtext = str(check.text)
+                    xetametni = str(sys.exc_info()[1])
+                    text = "**CYBΞR ERROR LOG**\n"
+                    link = "[CYBΞR Dəstək Qrupuna](https://t.me/TheCyberSupport)"
+                    if len(eventtext)<10:
+                        text += f"\n**⚙ Səbəb:** {eventtext}\n"
+                    text += "\n🔸️ İstəsəniz bunu bizə bildirə bilərsiniz."
+                    text += f" Sadəcə bu mesajı {link} göndərin.\n"
+                    text += "Xəta və tarix xaricində heç bir şey qeyd edilmir.\n"
+
+                    ftext = "========== XEBERDARLIQ =========="
+                    ftext += "\nBu fayl sadəcə bura yüklənib,"
+                    ftext += "\nSadəcə xəta və tarixi qeyd edirik,"
+                    ftext += "\nGizliliyiniz bizim üçün önəmlidir,"
+                    ftext += "\nBurada hər hansı bir gizli məlumat olarsa"
+                    ftext += "\nBu xəta bildirişi olmaz, heç kəs sizin məlumatlarınızı oğurlaya bilməz.\n"
+                    ftext += "--------USERBOT XƏTA LOG--------\n"
+                    ftext += "\nTarix: " + date
+                    ftext += "\nQrup ID: " + str(check.chat_id)
+                    ftext += "\nGöndərən adamın ID: " + str(check.sender_id)
+                    ftext += "\n\nƏmr:\n"
+                    ftext += str(check.text)
+                    ftext += "\n\nXəta mətni:\n"
+                    ftext += str(sys.exc_info()[1])
+                    ftext += "\n\n\nDaha ətraflı:\n"
+                    ftext += str(format_exc())
+                    ftext += "\n\n--------USERBOT XETA LOGU SON--------"
+                    ftext += "\n\n================================\n"
+                    ftext += f"====== ⚠️ Version : {CYBER_VERSION} ======\n"
+                    ftext += "================================"
+
+                    command = "git log --pretty=format:\"%an: %s\" -5"
+
+                    ftext += "\n\n\nSon 5 dəyişiklik:\n"
+
+                    process = await asyncsubshell(command,
+                                                  stdout=asyncsub.PIPE,
+                                                  stderr=asyncsub.PIPE)
+                    stdout, stderr = await process.communicate()
+                    result = str(stdout.decode().strip()) \
+                        + str(stderr.decode().strip())
+
+                    ftext += result
+
+                    file = open("cyber.log", "w+")
+                    file.write(ftext)
+                    file.close()
+
+                    if LOGSPAMMER:
+                        try:
+                            await check.client.send_message(check.chat_id, f"**{xetametni}**\n\n⌚️ **Tarix:** `{date}`\n\n❗️ **Əmr:** `{eventtext}`\n\n✅ **Xəta faylını** `BOTLOG` **qrupunuza göndərdim!**\n\n__CYBΞR ERROR LOG__")
+                        except:
+                            pass
+                    await check.client.send_file(send_to,
+                                                 "cyber.log",
+                                                 caption=text)
+
+                    remove("cyber.log")
+            else:
+                pass
+        if not disable_edited:
+            bot.add_event_handler(wrapper, events.MessageEdited(**args))
+        bot.add_event_handler(wrapper, events.NewMessage(**args))
+
+        return wrapper
+
+    return decorator
+
+
+def startcyberbot(shortname):
+    if shortname.startswith("__"):
+        pass
+    elif shortname.endswith("_"):
+        import importlib
+        import sys
+        from pathlib import Path
+
+        path = Path(f"userbot/cyber/{shortname}.py")
+        name = "userbot.cyber.{}".format(shortname)
+        spec = importlib.util.spec_from_file_location(name, path)
+        mod = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(mod)
+        print("Asistan qurulmağa hazırlanır..")
+        print(shortname + "modulu yükləndi")
+    else:
+        import importlib
+        import sys
+        from pathlib import Path
+
+        path = Path(f"userbot/cyber/{shortname}.py")
+        name = "userbot.cyber.{}".format(shortname)
+        spec = importlib.util.spec_from_file_location(name, path)
+        mod = importlib.util.module_from_spec(spec)
+        mod.tgbot = bot.tgbot
+        spec.loader.exec_module(mod)
+        sys.modules["userbot.cyber" + shortname] = mod
+        print(shortname + "modulu yükləndi") 
+
+
+def startcyberbot(shortname):
+    if shortname.startswith("__"):
+        pass
+    elif shortname.endswith("_"):
+        import importlib
+        import sys
+        from pathlib import Path
+
+        path = Path(f"userbot/cyber/{shortname}.py")
+        name = "userbot.cyber.{}".format(shortname)
+        spec = importlib.util.spec_from_file_location(name, path)
+        mod = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(mod)
+        print("Asistan qurulmağa hazırlanır..")
+        print(shortname + "modulu yükləndi")
+    else:
+        import importlib
+        import sys
+        from pathlib import Path
+
+        path = Path(f"userbot/cyber/{shortname}.py")
+        name = "userbot.cyber.{}".format(shortname)
+        spec = importlib.util.spec_from_file_location(name, path)
+        mod = importlib.util.module_from_spec(spec)
+        mod.tgbot = bot.tgbot
+        spec.loader.exec_module(mod)
+        sys.modules["userbot.cyber" + shortname] = mod
+        print("CYBΞR ASSISTANT" + shortname)  
---
+++ userbot/main.py
@@ -0,0 +1,338 @@
+# Copyright (C) 2021 Farid Dadashzade
+#
+# CYBERUSERBOT - FaridDadashzade
+
+import importlib
+from importlib import import_module
+from sqlite3 import connect
+import time
+import os
+import sys
+import requests
+import asyncio
+
+from telethon.tl.types import InputMessagesFilterDocument
+from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
+from telethon.tl.functions.channels import GetMessagesRequest
+from . import BRAIN_CHECKER, LOGS, bot, PLUGIN_CHANNEL_ID, CMD_HELP, LANGUAGE, CYBER_VERSION, PATTERNS, BOTLOG_CHATID, BOTLOG, StartTime, CYBER_BOT
+from .modules import ALL_MODULES
+import userbot.modules.sql_helper.mesaj_sql as MSJ_SQL
+import userbot.modules.sql_helper.galeri_sql as GALERI_SQL
+from pySmartDL import SmartDL
+from random import randint
+from telethon.tl import functions
+
+from random import choice
+import chromedriver_autoinstaller
+from json import loads, JSONDecodeError
+import re
+from asyncio import get_event_loop
+import userbot.cmdhelp
+
+from userbot import DEFAULT_NAME, SAHIB_ID, SON_GORULME
+from time import time
+import userbot.events
+
+CYBER_NAME = f"[{DEFAULT_NAME}](tg://user?id={SAHIB_ID})"
+QRUP = BOTLOG_CHATID
+
+def cyber_time(seconds, short=True):
+    minutes, seconds = divmod(int(seconds), 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    tmp = ((str(days) + (" gün, " if not short else "g, ")) if days else "") + \
+        ((str(hours) + (" saat, " if not short else "s, ")) if hours else "") + \
+        ((str(minutes) + (" dəqiqə, " if not short else "d, ")) if minutes else "") + \
+        ((str(seconds) + (" saniyə, " if not short else "s, ")) if seconds else "")
+    return tmp[:-2] + " əvvəl"
+
+notafk = round(time() - SON_GORULME)
+noe = cyber_time(notafk)
+noe2 = cyber_time(notafk, False)
+NO_AFK_TIME = noe2
+
+ALIVE_STR = [
+    "`C Y B Ξ R` {mention}-un `əmirlərinə hazırdır...`",
+    "`Hey` {mention} `narahat olma C Y B Ξ R əla işləyir...`",
+    "✨ `C Y B Ξ R` **{mention}**-in `əmirlərinə hazırdır...`",
+    "⛈️ {mention} `əlimdən gələnin ən yaxşısını etməyə hazıram...`",
+]
+
+
+LOGO_STR = [
+    "https://telegra.ph/file/c3e75eccaeb7f56dfae89.mp4",
+]
+
+
+KICKME_STR = [
+    "Bye bye mən gedirəm! 🦦",
+    "Qrupu tərk edirəm! 😒",
+    "Qrupdan çıxıram..\nBir daha dönməmək şərti ilə!",
+    "Qrupdan ayrılıram... 🦦",
+]
+
+NON_AFK = [
+    f"`Artıq AFK deyiləm.`",
+    f"{CYBER_NAME} `artıq AFK deyil.`",
+    f"{CYBER_NAME} `buradadır!`",
+    f"{CYBER_NAME} `gəldi!✨`",
+    f"{CYBER_NAME} `artıq sizinlədir!`",
+]
+
+DIZCILIK_STR = [
+    "Stikeri oğurlayıram...",
+    "Bu stikeri çox bəyəndimmm...",
+    "Bu stikeri öz paketimə əlavə edirəm...",
+    "Bunu oğurlamalıyamm...",
+    "Hey bu əla stikerdir!\nElə indi oğurlayıram..",
+    "Stikerini oğurladım\nhahaha.",
+    "Hey bura bax. (☉｡☉)!→\nMən bunu oğurlayarkən...",
+    "Bu stikeri paketimə əlavə edirəm...",
+    "Stiker paketə əlavə edilir...",
+    "Stikeri öz paketimə əlavə edirəm... ",
+]
+
+AFKSTR = [
+    "İndi vacib işim var, daha sonra mesaj atsan olmaz? Onsuzda yenə gələcəm.",
+    "Hörmətli istifadəçi zəng etdiyiniz şəxs hazırda telefona cavab verə bilmir.",
+    "Bir neçə dəqiqə içində gələcəm lakin gəlməsəm...\nbiraz daha gözlə.",
+    "İndi burada deyiləm..\nYəqin ki, başqa bir yerdəyəm..",
+    "Sahibim hal-hazırda AFK-dır!",
+    "Bəzən həyatdakı ən yaxşı şeylər gözləməyə dəyər…\nGələcəm.",
+    "Gələcəm,\namma əgər gəlməsəm,\ndaha sonra gələrəm.",
+    "Hal-hazırda sahibim burada deyil.\nXahiş edirəm biraz sonra əlaqə saxlayın.",
+    "Çox heyif ki sahibim burada deyil..",
+    "İndi burada deyiləm..\nTezliklə qayıdacağam..",
+    "Sahibim burada deyil..\nqayıdanda sizinlə əlaqə saxlayacaqdır.",
+    "Gələcəm,\namma əgər gəlməsəm,\ndaha sonra gələrəm.",
+    "Bir neçə dəqiqə içində gələcəm lakin gəlməsəm..\nbiraz daha gözlə.",
+    "Hey, sahibim hal-hazırda burada deyil..\nqayıtdığında sizinlə əlaqə saxlayacaqdır.",
+]
+
+UNAPPROVED_MSG = ("`Salam,` {mention} `\nBu bir avtomatik mesajdır.\nNarahat olma.\n\n`"
+                  "`Sahibim sənə PM yazma icazəsi verməyib. `"
+                  "`Zəhmət olmasa sahibimin aktiv olmasını gözləyin, o bəzən PM yazmağa icazə verər.\n\n`"
+                  "`Bildiyim qədəri ilə o beynini itirib insanlara PM icazəsi vermir.`")
+
+DB = connect("learning-data-root.check")
+CURSOR = DB.cursor()
+CURSOR.execute("""SELECT * FROM BRAIN1""")
+ALL_ROWS = CURSOR.fetchall()
+INVALID_PH = '\nXƏTA: Yazılan telefon nömrəsi yanlışdır' \
+             '\n  Tips: Ölkə kodunu istifadə edərək yenidən yaz' \
+             '\n       Telefon nömrənizi kontrol edin.'
+
+for i in ALL_ROWS:
+    BRAIN_CHECKER.append(i[0])
+connect("learning-data-root.check").close()
+
+def extractCommands(file):
+    FileRead = open(file, 'r').read()
+    
+    if '/' in file:
+        file = file.split('/')[-1]
+
+    Pattern = re.findall(r"@register\(.*pattern=(r|)\"(.*)\".*\)", FileRead)
+    Komutlar = []
+
+    if re.search(r'CmdHelp\(.*\)', FileRead):
+        pass
+    else:
+        dosyaAdi = file.replace('.py', '')
+        CmdHelp = userbot.cmdhelp.CmdHelp(dosyaAdi, False)
+
+       
+        for Command in Pattern:
+            Command = Command[1]
+            if Command == '' or len(Command) <= 1:
+                continue
+            Komut = re.findall("(^.*[a-zA-Z0-9şğüöçı]\w)", Command)
+            if (len(Komut) >= 1) and (not Komut[0] == ''):
+                Komut = Komut[0]
+                if Komut[0] == '^':
+                    KomutStr = Komut[1:]
+                    if KomutStr[0] == '.':
+                        KomutStr = KomutStr[1:]
+                    Komutlar.append(KomutStr)
+                else:
+                    if Command[0] == '^':
+                        KomutStr = Command[1:]
+                        if KomutStr[0] == '.':
+                            KomutStr = KomutStr[1:]
+                        else:
+                            KomutStr = Command
+                        Komutlar.append(KomutStr)
+
+           
+            Cyberpy = re.search('\"\"\"CYBERPY(.*)\"\"\"', FileRead, re.DOTALL)
+            if not Cyberpy is None:
+                Cyberpy = Cyberpy.group(0)
+                for Satir in Cyberpy.splitlines():
+                    if (not '"""' in Satir) and (':' in Satir):
+                        Satir = Satir.split(':')
+                        Isim = Satir[0]
+                        Deger = Satir[1][1:]
+                                
+                        if Isim == 'INFO':
+                            CmdHelp.add_info(Deger)
+                        elif Isim == 'WARN':
+                            CmdHelp.add_warning(Deger)
+                        else:
+                            CmdHelp.set_file_info(Isim, Deger)
+            for Komut in Komutlar:
+                # if re.search('\[(\w*)\]', Komut):
+                    # Komut = re.sub('(?<=\[.)[A-Za-z0-9_]*\]', '', Komut).replace('[', '')
+                CmdHelp.add_command(Komut, None, 'Bu plugin xaricdən yüklənib, hər hansı bir açıqlama qeyd olunmayıb.')
+            CmdHelp.add()
+
+try:
+    bot.start()
+    idim = bot.get_me().id
+    cyberbl = requests.get('https://raw.githubusercontent.com/FaridDadashzade/deploy/main/cyberbl.json').json()
+    if idim in cyberbl:
+        bot.send_message("me", f"`C Y B Ξ R adminləri tərəfindən botdan istifadə haqqınız alındı.`")
+        LOGS.error("C Y B Ξ R adminləri tərəfindən botunuz deaktiv edildi.")
+        bot.disconnect()
+
+    
+    try:
+        chromedriver_autoinstaller.install()
+    except:
+        pass
+    
+    
+    GALERI = {}
+
+    
+    PLUGIN_MESAJLAR = {}
+    ORJ_PLUGIN_MESAJLAR = {"alive": f"{str(choice(ALIVE_STR))}", "afk": f"`{str(choice(AFKSTR))}`", "kickme": f"{str(choice(KICKME_STR))}", "pm": UNAPPROVED_MSG, "dızcı": str(choice(DIZCILIK_STR)), "ban": "{mention}`, Banlandı!`", "mute": "{mention}`, səssizə alındı!`", "approve": "{mention}`, artıq mənə mesaj göndərə bilərsən!`", "disapprove": "{mention}`, artıq mənə mesaj göndərə bilməzsən!`", "block": "{mention}`, səni əngəllədim!`", "nonafk": f"{str(choice(NON_AFK))}", "salive": "https://telegra.ph/file/c3e75eccaeb7f56dfae89.mp4"}
+
+    PLUGIN_MESAJLAR_TURLER = ["alive", "afk", "kickme", "pm", "dızcı", "ban", "mute", "approve", "disapprove", "block", "nonafk", "salive"]
+    for mesaj in PLUGIN_MESAJLAR_TURLER:
+        dmsj = MSJ_SQL.getir_mesaj(mesaj)
+        if dmsj is False:
+            PLUGIN_MESAJLAR[mesaj] = ORJ_PLUGIN_MESAJLAR[mesaj]
+        else:
+            if dmsj.startswith("MEDYA_"):
+                medya = int(dmsj.split("MEDYA_")[1])
+                medya = bot.get_messages(PLUGIN_CHANNEL_ID, ids=medya)
+
+                PLUGIN_MESAJLAR[mesaj] = medya
+            else:
+                PLUGIN_MESAJLAR[mesaj] = dmsj
+    if not PLUGIN_CHANNEL_ID is None:
+        LOGS.info("Pluginlər yüklənir...")
+        try:
+            KanalId = bot.get_entity(PLUGIN_CHANNEL_ID)
+        except:
+            KanalId = "me"
+
+        for plugin in bot.iter_messages(KanalId, filter=InputMessagesFilterDocument):
+            if plugin.file.name and (len(plugin.file.name.split('.')) > 1) \
+                and plugin.file.name.split('.')[-1] == 'py':
+                Split = plugin.file.name.split('.')
+
+                if not os.path.exists("./userbot/modules/" + plugin.file.name):
+                    dosya = bot.download_media(plugin, "./userbot/modules/")
+                else:
+                    LOGS.info("Bu plugin onsuzda yüklüdür " + plugin.file.name)
+                    extractCommands('./userbot/modules/' + plugin.file.name)
+                    dosya = plugin.file.name
+                    continue 
+                
+                try:
+                    spec = importlib.util.spec_from_file_location("userbot.modules." + Split[0], dosya)
+                    mod = importlib.util.module_from_spec(spec)
+
+                    spec.loader.exec_module(mod)
+                except Exception as e:
+                    LOGS.info(f"`Yükləmədə problem! Plugin xətalıdır.\n\nXəta: {e}`")
+
+                    try:
+                        plugin.delete()
+                    except:
+                        pass
+
+                    if os.path.exists("./userbot/modules/" + plugin.file.name):
+                        os.remove("./userbot/modules/" + plugin.file.name)
+                    continue
+                extractCommands('./userbot/modules/' + plugin.file.name)
+    else:
+        bot.send_message("me", f"`Xahiş edirəm pluginlerin qalıcı olması üçün PLUGIN_CHANNEL_ID'i ayarlayın.`")
+except PhoneNumberInvalidError:
+    print(INVALID_PH)
+    sys.exit(1)
+    
+async def get_readable_time(seconds: int) -> str:
+    count = 0
+    up_time = ""
+    time_list = []
+    time_suffix_list = ["saniyə", "dəqiqə", "saat", "gün"]
+
+    while count < 4:
+        count += 1
+        remainder, result = divmod(seconds, 60) if count < 3 else divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        up_time += time_list.pop() + ", "
+
+    time_list.reverse()
+    up_time += ", ".join(time_list)
+
+    return up_time
+
+
+async def startupcyber():
+    try:
+        if QRUP != 0:
+            await bot.send_message(
+                QRUP,
+                f"Salam! Mən C Y B Ξ R UserBot\nBotumuzu qurduğunuz üçün təşəkkür edirəm!\nBotunuz aktivdir.\n\nC Y B Ξ R Version: **{CYBER_VERSION}**\n**Plugin sayı: {len(CMD_HELP)}**\n**Sahib: {CYBER_NAME}**\n\nYardıma ehtiyyacınız olarsa @TheCyberSupport qrupuna yazın :)",
+            )
+    except Exception as e:
+        LOGS.info(str(e))
+    
+    
+    
+async def cyberasistan():
+    if CYBER_BOT == "ON":
+        path = "userbot/cyber/*.py"
+        files = glob.glob(path)
+        for name in files:
+            with open(name) as f:
+                path1 = Path(f.name)
+                shortname = path1.stem
+                startcyberbot(shortname.replace(".py", ""))
+    
+
+async def FotoDegistir (foto):
+    FOTOURL = GALERI_SQL.TUM_GALERI[foto].foto
+    r = requests.get(FOTOURL)
+
+    with open(str(foto) + ".jpg", 'wb') as f:
+        f.write(r.content)    
+    file = await bot.upload_file(str(foto) + ".jpg")
+    try:
+        await bot(functions.photos.UploadProfilePhotoRequest(
+            file
+        ))
+        return True
+    except:
+        return False
+
+for module_name in ALL_MODULES:
+    imported_module = import_module("userbot.modules." + module_name)
+    
+loop = asyncio.get_event_loop()
+LOGS.info("Botunuz işləyir! Hər-hansısa bir söhbətə .alive yazaraq test edin."
+          " Köməyə ehtiyacınız olarsa, dəstək qrupumuza gəlin t.me/TheCyberSupport")
+LOGS.info(f"C Y B Ξ R {CYBER_VERSION}")
+#bot.loop.run_until_complete(cyberasistan())
+bot.loop.create_task(startupcyber())
+bot.run_until_disconnected()
